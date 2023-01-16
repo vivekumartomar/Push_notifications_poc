c8e02903740cae4556cac125ab3524274a000b7c
vivekumartomar <ervivekumar01@gmail.com>
2023-01-16 14:52:29 +0530

    Added push down notifications

---
+++ app/Notifications/PushDemo.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\WebPush\WebPushMessage;
+use NotificationChannels\WebPush\WebPushChannel;
+
+class PushDemo extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function via($notifiable)
+    {
+        return [WebPushChannel::class];
+    }
+
+    public function toWebPush($notifiable, $notification)
+    {
+        return (new WebPushMessage())
+            ->title('I\'m Notification Title')
+            ->icon('/notification-icon.png')
+            ->body('Great, Push Notifications work!')
+            ->action('View App', 'notification_action');
+    }
+    /**
+     * Send Push Notifications to all users.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function push()
+    {
+        Notification::send(User::all(), new PushDemo());
+        return redirect()->back();
+    }
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage())
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+                //
+            ];
+    }
+}
